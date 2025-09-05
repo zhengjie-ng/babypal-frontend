1a98068e935beec08431e814429760240f07db2b
Ng Zheng Jie <zhengjie.zjie@gmail.com>
2025-09-05 13:51:06 +0800

    feat: add deploy

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_CLIENT_URL: string
+  // Add other env variables here if you have any
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
